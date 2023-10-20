1001c0f1f331922e57706828d8fe60080485965c
LCoutinho7 <rm551617@fiap.com.br>
2023-10-19 22:19:52 -0300

    Tailwind basics

--- src/components/Cabecalho/cabecalho.jsx
+++ src/components/Cabecalho/cabecalho.jsx
@@ -4,13 +4,19 @@ import Link from "next/link"
 export default function Cabecalho() {
   return (
     <>
-      <div>
-        <ul>
-          <li><Link href="/produtos/tenis">TÊNIS</Link></li>
-          <li><Link href="/produtos/meia">MEIAS</Link></li>
-          <li><Link href="/produtos/camisa">CAMISAS</Link></li>
-        </ul>
-      </div>
+    <header className='bg-slate-800 text-white'>
+      <h1 className='text-center text-3x1 font-extrabold'> Produtos </h1> 
+      <hr className='bg-red-600 border-red-700'/>
+        <nav className='menu'>
+          <div>
+            <ul>
+              <li><Link href="/produtos/tenis">TÊNIS</Link></li>
+              <li><Link href="/produtos/meia">MEIAS</Link></li>
+              <li><Link href="/produtos/camisa">CAMISAS</Link></li>
+            </ul>
+          </div>
+        </nav>
+    </header>
     </>
   )
 }
